fe9513389742a69e754c0e4390571bf5ef9bb423
Lucas Maykot <lmaykot@gmail.com>
2026-03-31 15:32:53 -0300

    auto: update 2026-03-31 15:32:53

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AppShell } from './layouts/AppShell'
+import { CadastroCliente } from './pages/CadastroCliente'
+import { CadastroContrato } from './pages/CadastroContrato'
+import { GestaoPagamentos } from './pages/GestaoPagamentos'
+import { Relatorio } from './pages/Relatorio'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AppShell>
+        <Routes>
+          <Route path="/clientes" element={<CadastroCliente />} />
+          <Route path="/contratos" element={<CadastroContrato />} />
+          <Route path="/pagamentos" element={<GestaoPagamentos />} />
+          <Route path="/relatorio" element={<Relatorio />} />
+          <Route path="*" element={<Navigate to="/clientes" replace />} />
+        </Routes>
+      </AppShell>
+    </BrowserRouter>
+  )
+}
